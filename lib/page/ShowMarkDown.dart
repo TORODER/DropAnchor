2c6a272a78a553fcdc4a9374176a86970086f5ea
autop <1119134719@qq.com>
2021-03-15 22:30:59 +0800

    init

--- lib/page/ShowMarkDown.dart
+++ lib/page/ShowMarkDown.dart
@@ -1,7 +1,7 @@
 import 'package:drop_anchor/tool/SecuritySetState.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_markdown/flutter_markdown.dart';
-
+import 'package:url_launcher/url_launcher.dart';
 import '../mddata.dart';
 
 class ShowMarkDown extends StatefulWidget {
@@ -16,13 +16,25 @@ class ShowMarkDownState extends SecurityState<ShowMarkDown> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         title: Text('is MarkDown Title'),
+      ),
+      drawer: Container(
+        color: Colors.white,
+        width: 300,
+        height: double.infinity,
+        child: Column(
+          children: [],
+        ),
       ),
       body: Markdown(
         data: v1mdstr,
         selectable: true,
         physics: BouncingScrollPhysics(),
         onTapLink: (String text, String? href, String title) {
+          if (href != null) {
+            launch(href);
+          }
           print([
             text,
             href ?? null,
